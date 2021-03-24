1ba774cc14c0046ccbf939c791f7df917a8d10d4
Débora Picado <dbpicado@gmail.com>
2021-03-24 00:50:35 +0000

    carga de correciones

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Card from "../component/personajes";
+import CardPlanetas from "../component/planetas";
+import "../../styles/home.scss";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Star Wars</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Favorites</button>
+		<div className="container  bg-light">
+			<nav className="navbar navbar-light mb-3">
+				<Link className="col-6 col-md-2" to="/">
+					<img
+						className="img-fluid"
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1200px-Star_wars2.svg.png"
+					/>
 				</Link>
-			</div>
-		</nav>
+				<div className="col-3">
+					<div className="dropdown">
+						<button
+							className="btn btn-primary dropdown-toggle"
+							type="button"
+							id="dropdownMenuButton1"
+							data-bs-toggle="dropdown"
+							aria-expanded="false">
+							Favoritos
+							<span className="badge bg-secondary">{store.favorites.length}</span>
+						</button>
+						<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+							<li>
+								<a className="dropdown-item" href="#">
+									Action
+								</a>
+							</li>
+							<li>
+								<a className="dropdown-item" href="#">
+									Another action
+								</a>
+							</li>
+							<li>
+								<a className="dropdown-item" href="#">
+									Something else here
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,24 +1,48 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useContext } from "react";
+import Card from "../component/personajes";
+import CardPlanetas from "../component/planetas";
 import "../../styles/home.scss";
-import Card from "../component/card";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Home = () => {
-	let planets = [
-		{ nombre: "Saturno", superficie: 1000 },
-		{ nombre: "Marte", superficie: 2000 },
-		{ nombre: "Tierra", superficie: 3000 },
-		{ nombre: "Plutón", superficie: 500 }
-	];
+	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Home</h1>
-			<div className="row">
-				{planets.map((item, index) => {
+		<div className="container">
+			<div className="col-12">
+				<h1 className="text-danger">Personajes</h1>
+			</div>
+			<div className="row overX">
+				{store.personajes.map((item, index) => {
 					return (
-						<div className="col" key={index}>
-							<Card key={index} nombre={item.nombre} superficie={item.superficie} />
+						<div key={index} className="col-12 col-md-6 col-lg-4">
+							<Card
+								key={index}
+								name={item.name}
+								gender={item.gender}
+								hair_color={item.hair_color}
+								eye_color={item.eye_color}
+							/>
+						</div>
+					);
+				})}
+			</div>
+			<div className="col-12 mt-5">
+				<h1 className="text-danger">Planetas</h1>
+			</div>
+			<div className="row overX">
+				{store.planetas.map((item, index) => {
+					return (
+						<div key={index} className="col-12 col-md-6 col-lg-4 ">
+							<CardPlanetas
+								key={index}
+								url={item.url}
+								name={item.name}
+								diameter={item.diameter}
+								population={item.population}
+								terrain={item.terrain}
+							/>
 						</div>
 					);
 				})}
